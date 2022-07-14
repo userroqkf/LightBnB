3555d38afada435d0a225d901f44095db60f2924
John Kim <96427720+userkimjh@users.noreply.github.com>
2022-07-14 16:19:13 +0800

    add property to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -104,23 +104,27 @@ const getAllReservations = function (guest_id, limit = 10) {
     });
 };
 
+const addProperty = function(property) {
+  const {owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,
+    street,city,province,post_code,country,parking_spaces,number_of_bathrooms,
+    number_of_bedrooms} = property;
+  const text = `
+  INSERT INTO properties 
+  (owner_id,title,description,thumbnail_photo_url,cover_photo_url,
+    cost_per_night,street,city,province,post_code,country,parking_spaces,
+    number_of_bathrooms,number_of_bedrooms) VALUES ($1, $2, $3, $4 , $5, $6, $7,
+      $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+  const values = [owner_id,title,description,thumbnail_photo_url,
+    cover_photo_url,cost_per_night,street,city,province,post_code,country,
+    parking_spaces,number_of_bathrooms,number_of_bedrooms];
+  return pool.
+    query(text, values)
+    .then(result => result.rows);
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 exports.getUserWithId = getUserWithId;
 exports.addUser = addUser;
 exports.getAllProperties = getAllProperties;
 exports.getAllReservations = getAllReservations;
-
-
-
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-};
 exports.addProperty = addProperty;
